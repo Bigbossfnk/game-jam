524439d6bb17f87c44db1a7a1b68124bf35cb259
Bigbossfnk <Bigbossfnk@users.noreply.github.com>
2015-07-30 14:28:30 -0700

    FUCK

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -7,24 +7,32 @@ namespace Completed
 {
 	public class BoardManager : MonoBehaviour
 	{
-		public int columns = 8;
-		public int rows = 8;
+		public int columns;
+		public int rows;
 		// public GameObject exit;
 		public GameObject[] boardTiles;
 		// public GameObject[] cornerTiles;
 		// public GameObject[] riverTiles;
+		public Transform cameraTarget;
 
 		// used as parent to organize spawned tiles
-		private Transform boardHolder;
+		public Transform boardHolder;
 
 		// contains the possible spawn locations for the tiles
-		private List <Vector3> gridPositions = new List <Vector3> ();  
+		private List <Vector3> gridPositions = new List <Vector3> ();
+		private Transform rotationPivot;
 		
 		// clears gridPositions and generates a new board
 		void InitialiseList()
 		{
 			gridPositions.Clear ();
-			
+
+			columns = 8;
+			rows = 8;
+
+			// columns = Random.Range(8, 16);
+			// rows = Random.Range(8, 16);
+
 			// loop through x axis (columns)
 			for(int x = 0; x < (columns * 3) - 1; x += 3)
 			{
@@ -40,7 +48,8 @@ namespace Completed
 		void BoardSetup()
 		{
 			// instantiate new Board GameObject
-			boardHolder = new GameObject("Board").transform;
+			boardHolder.transform.position = new Vector3((float)(columns * 3) / 2, 0, (float)(rows * 3) / 2);
+			cameraTarget.transform.position = new Vector3((float)((columns * 3) / 2) - 3, 0, (float)((rows * 3) / 2) - 3);
 
 			for(int i = 0; i < gridPositions.Count; i++)
 			{
@@ -50,79 +59,46 @@ namespace Completed
 
 				instance.transform.SetParent(boardHolder);
 			}
+
+			// First find a center for your bounds.
+			Vector3 center = Vector3.zero;
+			
+			foreach (Transform child in boardHolder.transform)
+			{
+
+				center += child.GetComponent<Renderer>().bounds.center;
+			}
+			center /= boardHolder.transform.childCount; //center is average center of children
+			
+			//Now you have a center, calculate the bounds by creating a zero sized 'Bounds', 
+			Bounds bounds = new Bounds(center,Vector3.zero); 
+			
+			foreach (Transform child in boardHolder.transform)
+			{
+				bounds.Encapsulate(child.GetComponent<Renderer>().bounds);
+			}
+
+				                   Debug.Log (bounds.size.x / 2);
+				                   Debug.Log (bounds.size.z / 2);
 		}
 
-		void Start()
+		void Awake()
 		{
 			InitialiseList();
 			BoardSetup();
+
+			Debug.Log("Columns: " + columns);
+			Debug.Log("Rows   : " + rows);
 		}
-		
-		/*
-		//RandomPosition returns a random position from our list gridPositions.
-		Vector3 RandomPosition ()
-		{
-			//Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
-			int randomIndex = Random.Range (0, gridPositions.Count);
-			
-			//Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
-			Vector3 randomPosition = gridPositions[randomIndex];
-			
-			//Remove the entry at randomIndex from the list so that it can't be re-used.
-			gridPositions.RemoveAt (randomIndex);
-			
-			//Return the randomly selected Vector3 position.
-			return randomPosition;
-		}
-		*/
-		
-		/*
-		//LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create.
-		void LayoutObjectAtRandom (GameObject[] tileArray, int minimum, int maximum)
-		{
-			//Choose a random number of objects to instantiate within the minimum and maximum limits
-			int objectCount = Random.Range (minimum, maximum+1);
-			
-			//Instantiate objects until the randomly chosen limit objectCount is reached
-			for(int i = 0; i < objectCount; i++)
-			{
-				//Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
-				Vector3 randomPosition = RandomPosition();
-				
-				//Choose a random tile from tileArray and assign it to tileChoice
-				GameObject tileChoice = tileArray[Random.Range (0, tileArray.Length)];
-				
-				//Instantiate tileChoice at the position returned by RandomPosition with no change in rotation
-				Instantiate(tileChoice, randomPosition, Quaternion.identity);
+
+		int speed = 50;
+		void Update(){
+			if (Input.GetKey(KeyCode.LeftArrow)){
+				boardHolder.transform.RotateAround (cameraTarget.transform.position, Vector3.up, speed * Time.deltaTime);
+			}
+			if (Input.GetKey(KeyCode.RightArrow)){
+				boardHolder.transform.RotateAround (cameraTarget.transform.position, Vector3.up, -speed * Time.deltaTime);
 			}
 		}
-		*/
-
-		/*
-		//SetupScene initializes our level and calls the previous functions to lay out the game board
-		public void SetupScene (int level)
-		{
-			//Creates the outer walls and floor.
-			BoardSetup ();
-			
-			//Reset our list of gridpositions.
-			InitialiseList ();
-
-			//Instantiate a random number of wall tiles based on minimum and maximum, at randomized positions.
-			LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
-			
-			//Instantiate a random number of food tiles based on minimum and maximum, at randomized positions.
-			LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
-			
-			//Determine number of enemies based on current level number, based on a logarithmic progression
-			int enemyCount = (int)Mathf.Log(level, 2f);
-			
-			//Instantiate a random number of enemies based on minimum and maximum, at randomized positions.
-			LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
-			
-			//Instantiate the exit tile in the upper right hand corner of our game board
-			Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
-		}
-		*/
 	}
 }
